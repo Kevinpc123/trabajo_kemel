82b44f2886379d6b66081f8a7030890c5fe7a4c1
Kevinpc123 <pradokevin07@gmail.com>
2024-11-08 10:06:50 +0100

    Actualizacion v0.2 css inicio agregado de carrito y producto dto

--- modelos/usuarioDAO.php
+++ modelos/usuarioDAO.php
@@ -1,4 +1,31 @@
 <?php
-include '../database/conexion.php';
-include '../modelos/usuarioDTO.php';
-?>
+include_once 'config/database.php';
+include_once 'Usuario.php';
+
+class UsuarioDAO {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = (new Database())->getConnection();
+    }
+
+    public function registrarUsuario($usuario) {
+        $query = "INSERT INTO usuarios (username, passwordHash, nombre, apellido) VALUES (:username, :passwordHash, :nombre, :apellido)";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':username', $usuario->getUsername());
+        $stmt->bindParam(':passwordHash', $usuario->getPasswordHash());
+        $stmt->bindParam(':nombre', $usuario->getNombre());
+        $stmt->bindParam(':apellido', $usuario->getApellido());
+
+        return $stmt->execute();
+    }
+
+    public function obtenerUsuarioPorNombre($username) {
+        $query = "SELECT * FROM usuarios WHERE username = :username";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        return $stmt->fetchObject('Usuario');
+    }
+}
